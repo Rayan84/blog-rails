eafa827b4fb6b79702cfa6a2403ba6bd8d14cd98
Rayan84 <rayan_84@live.com>
2022-02-03 20:05:02 +0100

    fix rubocop issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  # get 'posts/index'
+  # get 'posts/show'
+  get 'users/index'
+  get 'users/show'
+
+  resource :users, only: %i[index show] do
+    resource :posts, only: %i[index show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
