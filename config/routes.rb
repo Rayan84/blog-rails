457c78de50402f2983e888b7a9bd62caed9a05e2
Rayan84 <rayan_84@live.com>
2022-02-04 23:05:52 +0100

    Add rspec gem and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get 'users/index'
-  get 'users/show'
-
-  resource :users, only: %i[index show] do
-    resource :posts, only: %i[index show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
