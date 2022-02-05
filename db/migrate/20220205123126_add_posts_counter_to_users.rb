aabefc204a2da4a362b9b0ab74d495f43ef17cdb
Rayan84 <rayan_84@live.com>
2022-02-05 14:04:33 +0100

    Add models user, post and comments

---
+++ db/migrate/20220205123126_add_posts_counter_to_users.rb
@@ -0,0 +1,4 @@
+class AddPostsCounterToUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20220205123556_add_likes_counter_to_posts.rb
@@ -0,0 +1,4 @@
+class AddLikesCounterToPosts < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
