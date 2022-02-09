bdb36d90bcfe404e239e5a3d3bdad4eb2893050a
Rayan84 <rayan_84@live.com>
2022-02-09 22:36:01 +0100

    Remove unnedded code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ApplicationRecord
-  # def recent_posts
-  #   @recent_posts_lists = []
-  #   User.all.each do |each_user|
-  #     each_user.posts.each do |each_post|
-  #      @recent_posts_lists.push(each_user.each_post.title)
-  #     end
-  #   end
-  # end
+
   def self.recent_posts(parameters)
     @recent_posts = Post.limit(3).where(author_id: parameters).order(created_at: :desc)
   end
