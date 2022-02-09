accfaa8405748df4e63cdd77ebd2f5dc698a7fd6
Rayan84 <rayan_84@live.com>
2022-02-09 18:02:41 +0100

    Move recent_posts function from the users controller to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
   
-  def recent_posts
-    @recent_posts_lists = []
-    User.all.each do |each_user|
-      each_user.posts.each do |each_post|
-       @recent_posts_lists.push(each_user.each_post.title)
-      end
-    end
+  # def recent_posts
+  #   @recent_posts_lists = []
+  #   User.all.each do |each_user|
+  #     each_user.posts.each do |each_post|
+  #      @recent_posts_lists.push(each_user.each_post.title)
+  #     end
+  #   end
+  # end
+  def self.recent_posts(parameters)
+    @recent_posts = Post.limit(3).where(author_id: parameters).order(created_at: :desc)
   end
+
 
   has_many :posts
   has_many :likes
