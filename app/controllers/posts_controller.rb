1342dde08e16800cf88599def1defb734c84f2c1
Rayan84 <rayan_84@live.com>
2022-02-09 16:24:51 +0100

    Add logic to render user's 3 most recent posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = User.all.find(params[:user_id])
+    @last_user = User.last
     @posts = []
-    # @ids = []
-    # @parameter = params[:user_id]
-    # @note = []
-
     Post.all.each do |post|
-    #  @note.push("looping #{post.author_id} #{params[:user_id]}")
       if post.author_id.to_s == params[:user_id]
-    #    @note.push('if')
-    #    @ids.push(post.author_id)
         @posts.push(post.title)
-      #find_by(author: params[:id])
       end
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+     @recent_posts = Post.limit(3).where(author_id: params[:id]).order(created_at: :desc)
   end
 end
+
+
+  #   @posts = []
+  #     Post.all.each do |post|
+  #     if post.author_id.to_s == params[:id]
+  #       @posts.push(post)
+  #      end
+  #     end
+  #  # @posts = Post.all
+  #    @posts = Post.all.find_by(author_id: params[:user_id])
