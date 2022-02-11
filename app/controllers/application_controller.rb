568e2f45d79b2d7a1a69c9387055b9f52c896ae9
Rayan84 <rayan_84@live.com>
2022-02-11 13:18:25 +0100

    Add logic to show posts count

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   def current_user
     User.first
   end
+
+  @posts_count = Post.count
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
     @user = User.all
+    @posts = Post.all
   end
 
   def show
+    @total_posts = Post.where(author_id: params[:id]).count
     @user = User.find(params[:id])
     @recent_posts = Post.limit(3).where(author_id: params[:id]).order(created_at: :desc)
   end
