246d3cc2825a90417518ef85bf207cdc31214d37
Rayan84 <rayan_84@live.com>
2022-02-12 21:51:50 +0100

    Add model tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  user1 = User.new
+  post1 = Post.new(title: 'post_test', text: 'this is post test', author: user1)
+  subject do
+    Like.new
+  end
+
+
 end
