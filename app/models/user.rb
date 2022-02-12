8d62ae400bcc25e5d600d787144303893d8efbe9
Rayan84 <rayan_84@live.com>
2022-02-12 10:34:16 +0100

    Remove unneccessary code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,4 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
-
-  def recent_post
-    @recent_posts = posts.order(created_at: :desc).limit(3)
-  end
 end
