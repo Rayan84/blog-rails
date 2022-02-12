a46c14a1b8d702b1153696da92ce7fcc20fc9502
Rayan84 <rayan_84@live.com>
2022-02-12 11:00:05 +0100

    Add new, create, edit and update resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show, :new, :create, :edit, :update] do
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   end
 end
